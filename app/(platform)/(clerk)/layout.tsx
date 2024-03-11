649d11600650b3914e28b72b0de06a0400711d6b
Vishnu Safaya <safayavishnu@gmail.com>
2024-03-11 16:58:32 +0530

    Changes to implement Clerk Authentication

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const ClerkLayout = ({children}: {children: React.ReactNode}) => {
+    return (
+        <div className="flex justify-center items-center h-full">
+            {children}
+        </div>
+    )
+}
+
+export default ClerkLayout;
